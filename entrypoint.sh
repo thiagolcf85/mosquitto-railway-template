80a483e0f31fe5348e794131081fea668de90adf
Lima-e-Silva <luizpaulo@protonmail.com>
2025-07-15 11:17:09 -0300

    🐛 Fix: Set ownership for password file
    Adds ownership change for the generated password file to ensure proper access control. The file is now owned by the "mosquitto" user and group, improving security and operational integrity within the container.

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,9 @@ fi
 # Cria o arquivo de senha a partir das variáveis de ambiente
 mosquitto_passwd -b -c /mosquitto/config/password_file "$MOSQUITTO_USERNAME" "$MOSQUITTO_PASSWORD"
 
+# Define "mosquitto" como proprietário do arquivo
+chown mosquitto:mosquitto /mosquitto/config/password_file
+
 # Passa a execução para o comando original do contêiner (inicia o mosquitto)
 # "$@" representa todos os argumentos passados para o script, que no nosso caso
 # será o comando para iniciar o broker definido no Dockerfile.
